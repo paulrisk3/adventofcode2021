9f706944c0b2bfd9927c5285b1cb32571694619b
Paul Risk <paulrisk3@gmail.com>
2021-12-05 17:31:32 -0500

    recursive. plz help.

--- day3.py
+++ day3.py
@@ -1,10 +1,27 @@
+def remove_non_matches(arr, match):
+    enum_arr = enumerate(arr)
+    
+    # remote non-matches
+    for idx, item in enum_arr:
+        if item[0] != match[0]:
+            arr.pop(idx)
+    print(match[0], len(arr), arr, "\n")
+
+    # if more than one result, do it again
+    if len(arr) > 1 and len(match) > 1:
+        remove_non_matches(arr, match[1:])
+        
+    return arr
+
 with open("input/day3_input.txt") as file:
+    initial_array = []
     rotated_array = []
 
     for i in file.readline().rstrip():
-            rotated_array.append([])
+        rotated_array.append([])
 
     while (line := file.readline().rstrip()):
+        initial_array.append(line)
         iter = 0
         line_array = []
         for char in line:
@@ -23,5 +40,10 @@ for j in rotated_array:
         gamma += '1'
         epsilon += '0'
 
+# print(initial_array)
+
 print("Gamma", gamma, int(gamma, 2))
 print("Epsilon", epsilon, int(epsilon,2 ))
+
+results = remove_non_matches(initial_array, '011010111101')
+# print("Results", results)
