45fd59a1edebb577fbbd065bb9be079d3c4e8201
Paul Risk <paulrisk3@gmail.com>
2021-12-05 19:42:30 -0500

    Iterates through chars in item now. It's like != isn't working...

--- day3.py
+++ day3.py
@@ -1,16 +1,16 @@
-def remove_non_matches(arr, match):
-    enum_arr = enumerate(arr)
-    
-    # remote non-matches
-    for idx, item in enum_arr:
-        if item[0] != match[0]:
-            arr.pop(idx)
-    print(match[0], len(arr), arr, "\n")
+def remove_non_matches(arr, match, count = 0):
+    print(match, arr)
+    # filter and remove non-matches
+    for item in arr:
+        if item[count] != match[0]:
+            arr.remove(item)
 
-    # if more than one result, do it again
+    # if more than one result in the array, do it again
     if len(arr) > 1 and len(match) > 1:
-        remove_non_matches(arr, match[1:])
-        
+        print("Run it again.")
+        count += 1
+        remove_non_matches(arr, match[1:], count)
+
     return arr
 
 with open("input/day3_input.txt") as file:
@@ -45,5 +45,26 @@ for j in rotated_array:
 print("Gamma", gamma, int(gamma, 2))
 print("Epsilon", epsilon, int(epsilon,2 ))
 
-results = remove_non_matches(initial_array, '011010111101')
-# print("Results", results)
+gamma_array = initial_array
+epsilon_array = initial_array
+
+test_array = [
+    '000100011010',
+    '110011110110',
+    '011000101111',
+    '001101100101',
+    '011100001000',
+    '101101011011',
+    '101111010101',
+    '011010000101',
+    '010101000010',
+    '100001111000'
+]
+
+# o2_results = remove_non_matches(gamma_array, gamma)
+# print("O2 Rating:", o2_results)
+# print(gamma_array)
+# co2_results = remove_non_matches(epsilon_array, epsilon)
+# print("CO2 Rating:", co2_results)
+results = remove_non_matches(test_array, '10000111')
+print(results)
