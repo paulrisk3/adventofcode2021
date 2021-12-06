bfc278af64f946db0157f0ac26ba3b158ec450c8
Paul Risk <paulrisk3@gmail.com>
2021-12-05 22:54:35 -0500

    Updated filter. Not done. Need to double-check recursion.

--- day3.py
+++ day3.py
@@ -1,9 +1,33 @@
+def rotate_array(arr):
+    rotated_arr = []
+
+    for i in arr[0]:
+        rotated_arr.append([])
+    
+    for item in arr:
+        iter = 0
+        for char in item:
+            rotated_arr[iter].append(int(char))
+            iter += 1
+    
+    return rotated_arr
+
 def remove_non_matches(arr, match, counter = 0):
-    arr = list(filter(lambda item: item[counter] == match[0], arr))
+    rotated_arr = rotate_array(arr)
+    # Find the filter by rotating the array and finding the most common number in the first bit.
+    # I think the 0 needs to be counter, instead.
+    if sum(rotated_arr[0])/len(rotated_arr[0]) > .5:
+        o2_filter = 1
+        co2_filter = 0
+    else:
+        o2_filter = 0
+        co2_filter = 1
+    arr = list(filter(lambda item: item[counter] == o2_filter, arr))
 
     # if more than one result in the array, do it again.
     if len(arr) > 1 and len(match) > 1:
-        arr = list(filter(lambda item: item[counter] == match[0], arr))
+        # uh oh. I probably shouldn't run this line twice in my recurisve function. That's not right.
+        arr = list(filter(lambda item: item[counter] == o2_filter, arr))
         counter += 1
         remove_non_matches(arr, match[1:], counter)
     else:
@@ -12,25 +36,19 @@ def remove_non_matches(arr, match, counter = 0):
 
 with open("input/day3_input.txt") as file:
     initial_array = []
-    rotated_array = []
-
-    for i in file.readline().rstrip():
-        rotated_array.append([])
-
+    
     while (line := file.readline().rstrip()):
         initial_array.append(line)
-        iter = 0
-        line_array = []
-        for char in line:
-            rotated_array[iter].append(int(char))
-            iter += 1
+
+    rotated_array = rotate_array(initial_array)
     
 gamma = ''
 epsilon = ''
 
 # build binary string
 for j in rotated_array:
-    if sum(j)/(len(j)+1) < .5:
+    print(sum(j)/(len(j)))
+    if sum(j)/(len(j)) < .5:
         gamma += '0'
         epsilon += '1'
     else:
@@ -42,25 +60,28 @@ for j in rotated_array:
 print("Gamma", gamma, int(gamma, 2))
 print("Epsilon", epsilon, int(epsilon,2 ))
 
-gamma_array = initial_array
-epsilon_array = initial_array
+# gamma_array = initial_array
+# epsilon_array = initial_array
 
 test_array = [
-    '000100011010',
-    '110011110110',
-    '011000101111',
-    '001101100101',
-    '011100001000',
-    '101101011011',
-    '101111010101',
-    '011010000101',
-    '010101000010',
-    '100001111000'
+    '00010001',
+    '11001111',
+    '01100010',
+    '00110110',
+    '01110000',
+    '10110101',
+    '10111101',
+    '01101000',
+    '01010100',
+    '10000111'
 ]
 
-o2_results = remove_non_matches(gamma_array, gamma)
-co2_results = remove_non_matches(epsilon_array, epsilon)
-# results = remove_non_matches(test_array, '10000111')
-# print("Results:", results)
-print("O2 Results:", o2_results)
-print("CO2 Results:", co2_results)
+# So, we can't filter just by gamma. The first bit in gamma is correct, but after that,
+# we need to recalculate. How do I calculate recursively...
+# Do I need to rotate the newly-filtered array each time? I say yes.
+# o2_results = remove_non_matches(gamma_array, rotated_array)
+# co2_results = remove_non_matches(epsilon_array, rotated_array)
+# # results = remove_non_matches(test_array, '10000111')
+# # print("Results:", results)
+# print("O2 Results:", o2_results)
+# print("CO2 Results:", co2_results)
